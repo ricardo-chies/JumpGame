f0a43abea9f21e9a4be946ef3187e4e7fbca65e7
Ricardo chies <ricardochies5@gmail.com>
2023-05-16 14:51:15 -0300

    Implementação Pokemons

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
-const mario = document.querySelector('.mario');
+const pikachu = document.querySelector('.pikachu');
 const pipe = document.querySelector('.pipe');
 
 const jump = () => {
-    mario.classList.add('jump');
+    pikachu.classList.add('jump');
 
     setTimeout(() => {
-        mario.classList.remove('jump');
+        pikachu.classList.remove('jump');
     }, 500)
 }
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const pikachuPosition = +window.getComputedStyle(pikachu).bottom.replace('px', '');
 
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if(pipePosition <= 120 && pipePosition > 0 && pikachuPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+        pikachu.style.animation = 'none';
+        //mario.style.bottom = `${marioPosition}px`;
 
-        mario.src = './images/game-over.png';
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px';
+        pikachu.src = './images/pikachuGameOver.png';
+        pikachu.style.width = '120px';
+        pikachu.style.marginLeft = '50px';
 
         clearInterval(loop);
     }
